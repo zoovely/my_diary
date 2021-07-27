dc52ab5597b4aa5d5ef05b9041aec30dca626c12
zoovely <yon041128@naver.com>
2021-07-27 18:57:48 +0900

    07/27

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,8 @@
-$("button a").on("click", function() {
-    var name = $("#name").val();
+$("input[type=submit]").on("click", function() {
     var pw = $("#pw").val();
     var pw_check = $("#pw_check").val();
 
-    if(name=='' || pw=='' || pw_check=='') {
-        alert("빈칸을 모두 작성해주세요.");
-    } else {
-        if(!(pw===pw_check)) {
-            $("#pw_warn").css("display","block");
-        } else {
-            $("#pw_warn").css("display","none");
-            alert(name+"님 환영합니다! 로그인 페이지에서 로그인해주세요.");
-            location.href = "/";
-        }
+    if(!(pw===pw_check)) {
+        $("#pw_warn").css("display","block");
     }
 });
